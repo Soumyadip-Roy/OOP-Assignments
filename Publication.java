0bc030d74f01921f928c30ab710328c4d2039912
Soumyadip Roy <85121861+Soumyadip-Roy@users.noreply.github.com>
2022-12-05 23:07:42 +0530

    Assignment 2

--- Publication.java
+++ Publication.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Publication{
+public class Publication {
 
     Scanner sc = new Scanner(System.in);
 
@@ -8,9 +8,9 @@ public class Publication{
     double price;
     int copies;
 
-    double magSold, magWorth, BookSold, BookWorth;
+    static double magSold, magWorth, BookSold, BookWorth;
 
-    public void salesCopy(){
+    public void salesCopy() {
         System.out.println("Number of Publications Sold: " + (magSold + BookSold));
         System.out.println("Total Worth of Publications Sold: " + (BookWorth + magWorth));
         System.out.println("Number of Book Sold: " + BookSold);
@@ -20,39 +20,38 @@ public class Publication{
     }
 
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
 
         Publication p = new Publication();
         Book b = new Book();
         Mag m = new Mag();
 
+        System.out.println("1. Add Book");
+        System.out.println("2. Add Magazine");
+        System.out.println("3. Order Books");
+        System.out.println("4. Order Magazine");
+        System.out.println("5. Restock Books");
+        System.out.println("6. Restock Magazines");
+        System.out.println("7. Sale of Books");
+        System.out.println("8. Sale of Magazines");
+        System.out.println("9. Sale of Publication");
+        System.out.println("0. Exit");
+        
+
         boolean cont = true;
-        while(cont){
-            
-            System.out.println("\n1. Add Book");
-			System.out.println("2. Add Magazine");
-			System.out.println("3. Order Books");
-			System.out.println("4. Order Magazine");
-			System.out.println("5. Restock Books");
-			System.out.println("6. Restock Magazines");
-			System.out.println("7. Sale of Books");
-			System.out.println("8. Sale of Magazines");
-			System.out.println("9. Sale of Publication");
-			System.out.println("0. Exit\n");
-            System.out.print("Enter your choice: ");
-            
+        while (cont) {
+        	System.out.print("\nEnter your choice: ");
             int x;
             x = sc.nextInt();
-            
-            switch(x){
+
+            switch (x) {
                 case 1:
                     b.add();
                     break;
                 case 2:
                     m.add();
                     break;
-
                 case 3:
                     System.out.print("Enter the Number of copies you want: ");
                     int c;
@@ -93,22 +92,22 @@ public class Publication{
 
                 case 0:
                     System.out.println("\nThankyou for using the Program.");
-                    cont=false;
+                    cont = false;
                     break;
 
                 default:
                     System.out.println("\nInvalid choice !");
-            } 
+            }
         }
         sc.close();
-    }  
+    }
 }
 
-class Book extends Publication{
+class Book extends Publication {
     String author;
     int BookCopies;
-    
-    void add(){
+
+    void add() {
         System.out.print("Enter the Title of the Book: ");
         title = sc.next();
         System.out.print("Enter the Price of the Book: ");
@@ -118,11 +117,10 @@ class Book extends Publication{
         System.out.print("Enter the Author's Name: ");
         author = sc.next();
     }
-    void OrderCopies(int value){
-        if(value > BookCopies){
+    void OrderCopies(int value) {
+        if (value > BookCopies) {
             System.out.println("Not Enough Stock, copies left: " + BookCopies);
-        }
-        else{
+        } else {
             BookCopies -= value;
             BookSold += value;
             BookWorth += (value * price);
@@ -130,23 +128,23 @@ class Book extends Publication{
         }
     }
 
-    void restock(int s){
+    void restock(int s) {
         BookCopies += s;
         System.out.println("Books left: " + BookCopies);
     }
 
-    public void salesCopy(){
-        System.out.print("Total books sold: " + BookSold+"\n");
-        System.out.print("Total worth: " + BookWorth+"\n");
+    public void salesCopy() {
+        System.out.print("Total books sold: " + BookSold + "\n");
+        System.out.print("Total worth: " + BookWorth + "\n");
     }
 
 }
 
-class Mag extends Publication{
+class Mag extends Publication {
     int issue;
     int magCopies;
 
-    void add(){
+    void add() {
         System.out.print("Enter the title of the Magazine: ");
         title = sc.next();
         System.out.print("Enter the price of the Magazine: ");
@@ -157,11 +155,10 @@ class Mag extends Publication{
         issue = sc.nextInt();
     }
 
-    void OrderCopies(int value){
-        if(value > magCopies){
+    void OrderCopies(int value) {
+        if (value > magCopies) {
             System.out.print("Not Enough Stock, copies left: " + magCopies);
-        }
-        else{
+        } else {
             magCopies -= value;
             magSold += value;
             magWorth += (value * price);
@@ -169,13 +166,73 @@ class Mag extends Publication{
         }
     }
 
-    void restock(int s){
+    void restock(int s) {
         magCopies += s;
         System.out.println("Magazines left: " + magCopies);
     }
 
-    public void salesCopy(){
-        System.out.print("Total magazines sold: " + magSold+"\n");
-        System.out.print("Total worth: " + magWorth+"\n");
+    public void salesCopy() {
+        System.out.print("Total magazines sold: " + magSold + "\n");
+        System.out.print("Total worth: " + magWorth + "\n");
     }
+
 }
+
+//OUTPUT
+
+//1. Add Book
+//2. Add Magazine
+//3. Order Books
+//4. Order Magazine
+//5. Restock Books
+//6. Restock Magazines
+//7. Sale of Books
+//8. Sale of Magazines
+//9. Sale of Publication
+//0. Exit
+//
+//Enter your choice: 1
+//Enter the Title of the Book: NCERT
+//Enter the Price of the Book: 120
+//Enter the Number of copies: 12
+//Enter the Author's Name: Soumyadip
+//
+//Enter your choice: 3
+//Enter the Number of copies you want: 16
+//Not Enough Stock, copies left: 12
+//
+//Enter your choice: 3
+//Enter the Number of copies you want: 10
+//Books left: 2
+//Enter your choice: 5
+//Enter the number of copies you want to add: 16
+//Books left: 18
+//
+//Enter your choice: 2
+//Enter the title of the Magazine: Times
+//Enter the price of the Magazine: 50
+//Enter the Number of copies: 45
+//Enter the issue of the Magazine: 2022
+//
+//Enter your choice: 4
+//Enter the Number of copies you want: 30
+//Magazines left: 15
+//Enter your choice: 7
+//Total books sold: 10.0
+//Total worth: 1200.0
+//
+//Enter your choice: 8
+//Total magazines sold: 30.0
+//Total worth: 1500.0
+//
+//Enter your choice: 9
+//Number of Publications Sold: 40.0
+//Total Worth of Publications Sold: 2700.0
+//Number of Book Sold: 10.0
+//Total Worth of Books Sold: 1200.0
+//Number of Magazine Sold: 30.0
+//Total Worth of Magazine Sold: 1500.0
+//
+//Enter your choice: 0
+//
+//Thankyou for using the Program.
